8c612d205ac727a80555795f3b80312b69ef7dff
liubin <915249126@qq.com>
2020-10-26 19:37:32 +0800

    feat(template) day 01

--- study/gin/main.go
+++ study/gin/main.go
@@ -1,10 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "gin/router"
+import (
+	"github.com/gin-gonic/gin"
+	"testGin/router"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", get.Get)
+	r.GET("/get", get.NameQuery)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
--- study/gin/router/get.go
+++ study/gin/router/get.go
@@ -1,8 +1,12 @@
 package get
 
-func Get(c *gin.Context) {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func NameQuery(c *gin.Context) {
 	firstname := c.DefaultQuery("firstname", "Guest")
 	lastname := c.Query("lastname") // 是 c.Request.URL.Query().Get("lastname") 的简写
-
 	c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
 }

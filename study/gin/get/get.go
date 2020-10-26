386dc8919c5a807d2048a6481d3f392d4ca885ca
liubin <915249126@qq.com>
2020-10-26 17:16:16 +0800

    feat(template) day 01

---
+++ study/gin/get/get.go
@@ -0,0 +1,8 @@
+package get
+
+func Main(c *gin.Context) {
+	firstname := c.DefaultQuery("firstname", "Guest")
+	lastname := c.Query("lastname") // 是 c.Request.URL.Query().Get("lastname") 的简写
+
+	c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+}
